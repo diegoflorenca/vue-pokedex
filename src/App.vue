df819094ef9465c4c71a0b5616ece1da0df84406
diegoflorenca <diegoflorenca@gmail.com>
2023-03-11 09:49:16 -0800

    study: fetch and display data using options api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+	async mounted() {
+		const pokeData = await fetch('http://pokeapi.co/api/v2/pokedex/2/').then((response) => response.json());
+		this.pokemonList = pokeData.pokemon_entries;
+	},
+	data: () => ({
+		pokemonList: [],
+	}),
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>
+		<h1>Pokedex</h1>
+		<ul>
+			<li v-for="pokemon in pokemonList" :key="`poke-${index}`">
+				{{ pokemon.pokemon_species.name }}
+			</li>
+		</ul>
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
