f6cc72429356ecd5d6f6e95ee321b3b578c2b935
diegoflorenca <diegoflorenca@gmail.com>
2023-03-11 14:00:34 -0800

    feat: filter pokemons using input text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
 const pokemonList = ref([]);
+const filteredPokemonText = ref([]);
+
+const filteredPokemonList = computed(() => pokemonList.value.filter((pokemon) => pokemon.pokemon_species.name.includes(filteredPokemonText.value)));
 
 onMounted(async () => {
 	const pokeData = await fetch('http://pokeapi.co/api/v2/pokedex/2/').then((response) => response.json());
@@ -12,8 +15,10 @@ onMounted(async () => {
 <template>
 	<div>
 		<h1>Pokedex</h1>
+		<p>{{ filteredPokemonText }}</p>
+		<input type="text" v-model="filteredPokemonText" />
 		<ul>
-			<li v-for="pokemon in pokemonList" :key="`poke-${index}`">
+			<li v-for="pokemon in filteredPokemonList" :key="`poke-${pokemon.entry_number}`">
 				{{ pokemon.pokemon_species.name }}
 			</li>
 		</ul>
