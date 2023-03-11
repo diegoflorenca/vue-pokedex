b5f4aadfd85372a7b9e737b1654e3ecc0a325aff
diegoflorenca <diegoflorenca@gmail.com>
2023-03-11 14:59:27 -0800

    study: using reactive store

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { reactive, ref, onMounted, computed } from 'vue';
 
-const pokemonList = ref([]);
 const filteredPokemonText = ref([]);
 
-const filteredPokemonList = computed(() => pokemonList.value.filter((pokemon) => pokemon.pokemon_species.name.includes(filteredPokemonText.value)));
+const pokemonStore = reactive({
+	list: [],
+	filterePokemonList: computed(() =>
+		pokemonStore.list.filter((pokemon) =>
+			pokemon.pokemon_species.name.includes(filteredPokemonText.value)
+		)
+	)
+});
 
 onMounted(async () => {
-	const pokeData = await fetch('http://pokeapi.co/api/v2/pokedex/2/').then((response) => response.json());
-	pokemonList.value = pokeData.pokemon_entries;
+	const pokeData = await fetch('http://pokeapi.co/api/v2/pokedex/2/').then((response) =>
+		response.json()
+	);
+	pokemonStore.list = pokeData.pokemon_entries;
 });
 </script>
 
 <template>
 	<div>
 		<h1>Pokedex</h1>
-		<p>{{ filteredPokemonText }}</p>
+		<p>Filtered Text: {{ filteredPokemonText }}</p>
 		<input type="text" v-model="filteredPokemonText" />
 		<ul>
-			<li v-for="pokemon in filteredPokemonList" :key="`poke-${pokemon.entry_number}`">
+			<li v-for="(pokemon, index) in pokemonStore.filterePokemonList" :key="`poke-${index}`">
 				{{ pokemon.pokemon_species.name }}
 			</li>
 		</ul>
