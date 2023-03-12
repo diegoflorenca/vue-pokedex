f00603db1350c940609711caa3ec6210f4f7a021
diegoflorenca <diegoflorenca@gmail.com>
2023-03-11 18:24:13 -0800

    feat: add pagination

--- netlify/functions/pokedex/pokedex.js
+++ netlify/functions/pokedex/pokedex.js
@@ -1,8 +1,12 @@
-const handler = async (event) => {
+const handler = async (event, context) => {
 	try {
 		const pokeApiData = await fetch('http://pokeapi.co/api/v2/pokedex/2/').then((response) =>
 			response.json()
 		);
+
+		const { start, end } = JSON.parse(event.body);
+
+		let finalResult = [];
 
 		const pokeData = pokeApiData.pokemon_entries
 			? pokeApiData.pokemon_entries.map((pokemon) => {
@@ -16,9 +20,11 @@ const handler = async (event) => {
 			  })
 			: [];
 
+		finalResult = pokeData.slice(start, end);
+
 		return {
 			statusCode: 200,
-			body: JSON.stringify(pokeData)
+			body: JSON.stringify(finalResult)
 		};
 	} catch (error) {
 		return { statusCode: 500, body: error.toString() };
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,33 @@ import { reactive, ref, onMounted, computed } from 'vue';
 import PokedexCard from './components/PokedexCard.vue';
 
 const filteredPokemonText = ref([]);
+
+const paginationStore = reactive({
+	start: 0,
+	end: computed(() => paginationStore.start + 10)
+});
+
+const turnNextPage = () => {
+	paginationStore.start += 10;
+	getPokedexEntries();
+};
+
+const turnPreviousPage = () => {
+	paginationStore.start -= 10;
+	getPokedexEntries();
+};
+
+const getPokedexEntries = async () => {
+	const pokeData = await fetch('/.netlify/functions/pokedex/pokedex.js', {
+		method: 'POST',
+		body: JSON.stringify({
+			start: paginationStore.start,
+			end: paginationStore.end
+		})
+	}).then((response) => response.json());
+	pokemonStore.list = pokeData;
+};
+
 const pokemonStore = reactive({
 	list: [],
 	filterePokemonList: computed(() =>
@@ -10,29 +37,25 @@ const pokemonStore = reactive({
 	)
 });
 
-onMounted(async () => {
-	const pokeData = await fetch('/.netlify/functions/pokedex/pokedex.js').then((response) =>
-		response.json()
-	);
-	console.log(pokeData);
-	pokemonStore.list = pokeData;
+onMounted(() => {
+	getPokedexEntries();
 });
 </script>
 
 <template>
-	<div>
-		<h1>Pokedex</h1>
-		<p>Filtered: {{ pokemonStore.filterePokemonList.length }}</p>
-		<input type="text" v-model="filteredPokemonText" />
-		<ul>
-			<PokedexCard
-				v-for="(pokemon, index) in pokemonStore.filterePokemonList"
-				:key="`poke-${index}`"
-				:id="pokemon.id"
-				:name="pokemon.name"
-			/>
-		</ul>
-	</div>
+	<h1>Pokedex</h1>
+	<p>Filtered: {{ pokemonStore.filterePokemonList.length }}</p>
+	<input type="text" v-model="filteredPokemonText" />
+	<ul>
+		<PokedexCard
+			v-for="(pokemon, index) in pokemonStore.filterePokemonList"
+			:key="`poke-${index}`"
+			:id="pokemon.id"
+			:name="pokemon.name"
+		/>
+	</ul>
+	<button v-if="paginationStore.start > 0" @click="turnPreviousPage">Previous Page</button>
+	<button v-if="paginationStore.end < 151" @click="turnNextPage">Next Page</button>
 </template>
 
 <style scoped></style>
